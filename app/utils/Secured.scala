e0bdd9b8d4c2d8d46f24d0438057e524cbee89e6
Daniel Mateus Pires <dmateusp@gmail.com>
2017-05-18 19:40:09 +0100

    Removing some useless lines

--- app/utils/Secured.scala
+++ app/utils/Secured.scala
@@ -12,30 +12,12 @@ import play.api.Play.current
 import scala.concurrent._
 import scala.concurrent.ExecutionContext.Implicits.global
 
-
-
-trait Secured {
-  def verifier: Verifier
-  def Authenticated = new ActionBuilder[AuthenticatedRequest] {
-    override def invokeBlock[A](request: Request[A], block: AuthenticatedRequest[A] => Future[Result]) = {
-      println(request.headers)
-      request.headers.get("Authentication") match {
-        case Some(token) => {
-          println(verifier.verifyToken)
-          block(new AuthenticatedRequest(User(token, ""), request))
-        }
-        case _ => Future.successful(Unauthorized)
-      }
-    }  }
-}
-
 class AuthenticatedRequest[A](val user: User, request: Request[A]) extends WrappedRequest[A](request)
 
 class Authentication @Inject()(ws: WSClient, cache: CacheApi){
   object AuthenticatedAction extends ActionBuilder[AuthenticatedRequest] {
     val verifier = new Verifier(ws, cache)
     def invokeBlock[A](request: Request[A], block: AuthenticatedRequest[A] => Future[Result]) = {
-      //println(request.headers)
       request.headers.get("Authentication") match {
         case Some(token) => {
           verifier.verifyToken(token).map(isValidToken => if(isValidToken) println("Valid!") else println("Not valid!"))
@@ -49,9 +31,6 @@ class Authentication @Inject()(ws: WSClient, cache: CacheApi){
 
 
 case class Verifier(ws: WSClient, cache: CacheApi) {
-  /*val verifyToken: (String, URL) => Boolean = (token, url) => {
-    response: Future[WSResponse] = ws.url(url).withHeaders("Accept" -> "application/json").get()
-  }*/
   val baseUrl: String = current.configuration.getString("authenticationServer").getOrElse("")
   val getPublicKey: () => Future[String] = { () => {
       val url: String = baseUrl + "/auth/public-key"
